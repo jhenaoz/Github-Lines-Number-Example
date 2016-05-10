33b49c76037025c6d5a3d739ac44885ab7c1354d
Juan D Henao <jhenaoz93@gmail.com>
2016-05-10 15:57:54 -0500

    support to not ready request to github

--- src/main/java/co/com/psl/request/GithubWrapper.java
+++ src/main/java/co/com/psl/request/GithubWrapper.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Component;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
+import co.com.psl.exceptions.ReportNotReadyException;
+
 @Component
 public class GithubWrapper implements EnvironmentAware {
 
@@ -29,30 +31,51 @@ public class GithubWrapper implements EnvironmentAware {
 
 	public static final String GITHUB_API = "https://api.github.com/repos/";
 	public static final String GITHUB_API_CONTRIBUTORS = "/stats/contributors?access_token=" ;
-	public static final String GITHUB_API_TREES_RECURSIVE = "/git/trees/master?recursive=1&access_token=";
+	public static final String GITHUB_API_TREES_RECURSIVE = "/git/trees/";
 	public static final String GITHUB_API_ADDITIONS_PER_WEEK = "/stats/code_frequency";
 
-	public JsonArray getContributorsByRepo(String repository) throws IOException {
+
+	public JsonArray getContributorsByRepo(String repository) throws IOException, ReportNotReadyException {
 		StringBuilder getContributorsUrl = new StringBuilder()
 			.append(GITHUB_API)
 			.append(repository)
 			.append(GITHUB_API_CONTRIBUTORS)
-			.append(this.propertyResolver.getProperty("GITHUB_ACCESS_TOKEN"));
+			.append(this.propertyResolver.getProperty("GITHUB_ACCESS_TOKEN"))
+			.append("&")
+			.append(this.propertyResolver.getProperty("GITHUB_CLIENT_ID"))
+			.append("&")
+			.append(this.propertyResolver.getProperty("GITHUB_CLIENT_SECRET"));
 		String httpResponse = makeHttpRequest(getContributorsUrl.toString(), HttpMethod.GET);
 		return parser.parse(httpResponse).getAsJsonArray();
 	}
 	
-	public JsonArray getNumberOfFilesByRepository(String repository) throws IOException{
+	public String getDefaultBranch(String repository) throws IOException, ReportNotReadyException{
+		StringBuilder defaultBranchRequest = new StringBuilder()
+			.append(GITHUB_API)
+			.append(repository)
+			.append("?access_token=")
+			.append(this.propertyResolver.getProperty("GITHUB_ACCESS_TOKEN"));
+		String httpResponse = makeHttpRequest(defaultBranchRequest.toString(), HttpMethod.GET);
+		return parser.parse(httpResponse).getAsJsonObject().get("default_branch").getAsString();
+	}
+	
+	public JsonArray getNumberOfFilesByRepository(String repository) throws IOException, ReportNotReadyException{
 		StringBuilder getTreeFilesUrl = new StringBuilder()
 			.append(GITHUB_API)
 			.append(repository)
 			.append(GITHUB_API_TREES_RECURSIVE)
-			.append(this.propertyResolver.getProperty("GITHUB_ACCESS_TOKEN"));
+			.append(getDefaultBranch(repository))
+			.append("?recursive=1&access_token=")
+			.append(this.propertyResolver.getProperty("GITHUB_ACCESS_TOKEN"))
+			.append("&")
+			.append(this.propertyResolver.getProperty("GITHUB_CLIENT_ID"))
+			.append("&")
+			.append(this.propertyResolver.getProperty("GITHUB_CLIENT_SECRET"));
 		String httpResponse = makeHttpRequest(getTreeFilesUrl.toString(), HttpMethod.GET);
 		return parser.parse(httpResponse).getAsJsonObject().get("tree").getAsJsonArray();
 	}
 	
-	public JsonArray getNumberOfLinesModificationsFromDate(String repository, String date, String branch) throws IOException{
+	public JsonArray getNumberOfLinesModificationsFromDate(String repository, String date, String branch) throws IOException, ReportNotReadyException{
 		StringBuilder commitsUrl = new StringBuilder()
 			.append(GITHUB_API)
 			.append(repository)
@@ -61,10 +84,16 @@ public class GithubWrapper implements EnvironmentAware {
 		return parser.parse(httpResponse).getAsJsonArray();	
 	}
 	
-	public String makeHttpRequest(String url, HttpMethod method) throws IOException{
+	public String makeHttpRequest(String url, HttpMethod method) throws IOException, ReportNotReadyException{
 		URL urlToRequest = new URL(url);
 		HttpURLConnection httpConnection = (HttpURLConnection) urlToRequest.openConnection();
 		httpConnection.setRequestMethod(method.toString());
+		int statusCode = httpConnection.getResponseCode();
+		if (statusCode == 202) {
+			throw new ReportNotReadyException();
+		}else if(statusCode == 204){
+			return "[]";
+		}
 		InputStream inputStream = httpConnection.getInputStream();
 		StringWriter writer = new StringWriter();
 		IOUtils.copy(inputStream, writer);
--- src/main/java/co/com/psl/service/ReportGenerator.java
+++ src/main/java/co/com/psl/service/ReportGenerator.java
@@ -6,6 +6,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.annotation.PostConstruct;
 
@@ -15,38 +17,52 @@ import org.springframework.stereotype.Component;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
+import co.com.psl.exceptions.ReportNotReadyException;
 import co.com.psl.model.GithubRepository;
 import co.com.psl.request.GithubWrapper;
 
 @Component
 public class ReportGenerator {
 
-	
 	public static final String CSV_SEPARATOR = ",";
 	public static final int MONTHS_TO_SEARCH = 12;
+	Queue<String> pendingRequests = new LinkedBlockingQueue<String>();
 	@Autowired
 	private CvsParser cvsParser;
 
 	@Autowired
 	private GithubWrapper github;
-	
+
 	private ArrayList<GithubRepository> reposInformation = new ArrayList<GithubRepository>();
 
 	@PostConstruct
-	public void generateReposStatisticsReport() throws IOException {
+	public void generateReposStatisticsReport() throws IOException, InterruptedException {
 		String[] repos = cvsParser.loadCsvFile();
 		for (String repo : repos) {
-			String repoName = getRepoNameFromCvsLine(repo);
+			requestGithubReport(repo);
+		}
+		//give some time for pending github reports.
+		pendingRequests.forEach((repo)-> requestGithubReport(repo));
+		exportReportAsCsv("Repositories Report");
+		System.out.println("End Report Procces");
+		System.exit(0);
+	}
+
+	public void requestGithubReport(String repo){
+		String repoName = getRepoNameFromCvsLine(repo);
+		try {
 			int numberOfLines = getLinesNumberByRepo(repoName);
 			int numberOfFiles = getFilesByRepository(repoName);
 			int numberOfModifiedLines = getModifiedLinesInTheLastMonths(repoName, MONTHS_TO_SEARCH);
-			reposInformation.add(new GithubRepository(repoName, numberOfLines, numberOfFiles, numberOfModifiedLines ));
+			reposInformation.add(new GithubRepository(repoName, numberOfLines, numberOfFiles, numberOfModifiedLines));
+		} catch (ReportNotReadyException e) {
+			pendingRequests.add(repo);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		exportReportAsCsv("Repositories Report");
-		System.exit(0);
 	}
-	
-	private int getFilesByRepository(String repositoryName) throws IOException {
+
+	private int getFilesByRepository(String repositoryName) throws IOException, ReportNotReadyException {
 		JsonArray files = github.getNumberOfFilesByRepository(repositoryName);
 		return files.size();
 	}
@@ -62,18 +78,18 @@ public class ReportGenerator {
 			writer.append(CSV_SEPARATOR);
 			writer.append(String.valueOf(githubRepository.getNumberOfModifications()));
 			writer.append("\n");
-			
+
 		}
 		writer.flush();
 		writer.close();
 	}
 
-	public String getRepoNameFromCvsLine(String cvsLine){
-		String[] cvsElements = cvsLine.split(","); 
+	public String getRepoNameFromCvsLine(String cvsLine) {
+		String[] cvsElements = cvsLine.split(",");
 		return cvsElements[0];
 	}
 
-	public int getLinesNumberByRepo(String repo) throws IOException {
+	public int getLinesNumberByRepo(String repo) throws IOException, ReportNotReadyException {
 		int codeLines = 0;
 		JsonArray contributors = github.getContributorsByRepo(repo);
 		for (JsonElement contributor : contributors) {
@@ -85,20 +101,20 @@ public class ReportGenerator {
 		}
 		return codeLines;
 	}
-	
-	public int getModifiedLinesInTheLastMonths(String repo, int months) throws IOException{
+
+	public int getModifiedLinesInTheLastMonths(String repo, int months) throws IOException, ReportNotReadyException {
 		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.MONDAY, (months*-1));
+		calendar.add(Calendar.MONDAY, (months * -1));
 		String beginingDateForSearch = convertDateToGithubFormat(calendar);
 		JsonArray weeksOfWork = github.getNumberOfLinesModificationsFromDate(repo, beginingDateForSearch, "master");
-		int weeksToCalculateModifications = months*4; // 4 weeks per month;
+		int weeksToCalculateModifications = months * 4; // 4 weeks per month;
 		int modifiedLines = 0;
 		if (weeksToCalculateModifications > weeksOfWork.size()) {
 			for (JsonElement week : weeksOfWork) {
 				modifiedLines += Math.abs(week.getAsJsonArray().get(1).getAsInt());
 				modifiedLines += Math.abs(week.getAsJsonArray().get(2).getAsInt());
 			}
-		}else{
+		} else {
 			for (int i = 0; i < weeksToCalculateModifications; i++) {
 				JsonElement week = weeksOfWork.get(i);
 				modifiedLines += Math.abs(week.getAsJsonArray().get(1).getAsInt());
@@ -108,11 +124,11 @@ public class ReportGenerator {
 
 		return modifiedLines;
 	}
-	
-	public String convertDateToGithubFormat(Calendar calendar){
+
+	public String convertDateToGithubFormat(Calendar calendar) {
 		DateFormat dateFormatInIso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");
 		dateFormatInIso8601.setTimeZone(calendar.getTimeZone());
-		return  dateFormatInIso8601.format(calendar.getTime());
+		return dateFormatInIso8601.format(calendar.getTime());
 	}
 
 }
